c1edab7d5bef723d2bb64e40ab557eca297dc7a3
Rodrigo Martinez <jorbinaldo@gmail.com>
2025-04-01 09:20:50 -0300

    Update hubspot.py

--- hubspot.py
+++ hubspot.py
@@ -508,6 +508,7 @@ if dados:
                 yaxis_title='Quantidade',
                 legend_orientation='h',
                 legend_y=1.0,
+                font=dict(size=20),
                 xaxis_tickfont_size=12,
                 height=450,
                 width=1250,
